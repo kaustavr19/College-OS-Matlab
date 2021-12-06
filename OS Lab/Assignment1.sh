2ca76ec94ef6f775f08694240cfce7e3cacf4ad6
Kaustav Roy <kaustavr25@gmail.com>
2021-12-06 08:06:37 -0800

    Create Assignment1.sh

---
+++ OS Lab/Assignment1.sh
@@ -0,0 +1,11 @@
+<!-- List the names of the users logged in -->
+who
+
+<!-- Find out your terminal’s device name -->
+hostname
+
+<!-- Display current date in the form dd/mm/yy -->
+date +%D
+
+<!-- Find out your machine’s name and the version of the operating system -->
+cat /etc/os-release
